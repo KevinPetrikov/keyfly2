c97990f640c2fbf4ca7e85e912f46ad112d6f2c2
KevinPetrikov <47643163+KevinPetrikov@users.noreply.github.com>
2019-08-18 07:49:27 -0500

    Revert "2.0 Beta"
    This reverts commit 6c2dff5b37e8422860330456eb87f2ce5709b16f.

--- scripts/debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export KEYFLYDebug=1
-export KEYFLYWIKillProcesses=1
-export KEYFLYWIReloadDriver=1
-
--- scripts/preferences.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# export KEYFLYAuto=1
-# export KEYFLYDebug=1
-# export KEYFLYWIKillProcesses=1
-# export KEYFLYWIReloadDriver=1
